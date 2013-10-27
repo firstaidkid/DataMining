d51862da79dbf079f5d786eaf8f4b0f5efeba75a
Steffen Kolb <sapmme@gmail.com>
2013-10-27 22:29:04 +0100

    CHANGED boxplot to show 5 plots instead of 1

--- 01 Energy Data/src/energyStatistics.py
+++ 01 Energy Data/src/energyStatistics.py
@@ -9,7 +9,23 @@ described = data.describe(percentile_width)
 print described
 
 # Create a boxplot
-bp = data.boxplot()
-plt.show()
+#bp = data.boxplot(column=['Oil','Gas','Coal', 'Nuclear', 'Hydro'], vert=False)
+#plt.show()
 
-# Make boxbplot per Energy Source
+# Or make boxbplot per Energy Source
+plt.subplot(511)
+bp_oil = data.boxplot(column=['Oil'], vert=False)
+
+plt.subplot(512)
+bp_gas = data.boxplot(column=['Gas'], vert=False)
+
+plt.subplot(513)
+bp_coal = data.boxplot(column=['Coal'], vert=False)
+
+plt.subplot(514)
+bp_nuclear = data.boxplot(column=['Nuclear'], vert=False)
+
+plt.subplot(515)
+bp_hydro = data.boxplot(column=['Hydro'], vert=False)
+plt.subplots_adjust(left=None, bottom=None, right=None, top=None,wspace=None, hspace=0.5)
+plt.show()
