2b125c5e2b5eac6e5fd3c4374e8d8816b40bf8c3
Steffen Kolb <hi@steffenkolb.de>
2013-10-31 12:44:57 +0100

    CHANGED energyPrediction; ADDED result-images
    Finished Energy-Prediction

--- 01 Energy Data/src/energyPrediction.py
+++ 01 Energy Data/src/energyPrediction.py
@@ -6,6 +6,7 @@ from sklearn import cross_validation
 
 # Read data from CSV File
 energyDataFrame = pd.read_csv('../resources/EnergyMixGeo.csv')
+numKeys = len(energyDataFrame);
 
 # extract trainings data and target data 
 trainingData = np.array(energyDataFrame[['Oil', 'Coal', 'Gas', 'Nuclear', 'Hydro']])
@@ -14,18 +15,19 @@ targetData = energyDataFrame['CO2Emm']
 # create SVR
 svr = SVR(kernel='linear')
 
-# create kfold
-n_folds = 10
-kf = cross_validation.KFold(len(energyDataFrame), n_folds=n_folds)
+# NOT USED ANYMORE : cross_val_score supports Cross-Validation-Parameter, which does the same
+# # create kfold
+# n_folds = 10
+# kf = cross_validation.KFold(numKeys, n_folds=n_folds)
 
-# fill cross-validation
-for train_index, test_index in kf:
-	#print("TRAIN:", train_index, "TEST:", test_index)
-	X_train, X_test = trainingData[train_index], trainingData[test_index]
-	y_train, y_test = targetData[train_index], targetData[test_index]
+# # fill cross-validation
+# for train_index, test_index in kf:
+# 	#print("TRAIN:", train_index, "TEST:", test_index)
+# 	X_train, X_test = trainingData[train_index], trainingData[test_index]
+# 	y_train, y_test = targetData[train_index], targetData[test_index]
 
 # score
-score = cross_validation.cross_val_score(svr, trainingData, y=targetData, scoring ="mean_squared_error", cv=kf)
+score = cross_validation.cross_val_score(svr, trainingData, targetData, scoring ="mean_squared_error", cv=10)
 print score
 
 # start fitting
@@ -37,8 +39,11 @@ predictedData = svr.predict(trainingData)
 
 
 # calculate the absolute deviation
-absDeviation = (targetData.mean() - predictedData.mean());
-if absDeviation < 0:
-	absDeviation *= -1
+absDeviation = predictedData - targetData
+# print "Absolute Deviation: " + str(absDeviation)
+# plt.stem(np.arange(numKeys), absDeviation)
+# plt.title('Prediction Error')
+# plt.show()
 
-print "Absolute Deviation: " + str(absDeviation)
+plt.plot(np.arange(numKeys), predictedData, '-r', targetData, '-k')
+plt.show()
