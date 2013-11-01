4df5100e5fa55707a57a7c8269eeca3f19b1a852
Steffen Kolb <hi@steffenkolb.de>
2013-11-01 09:11:53 +0100

    UPDATED google-maps
    cleaned the code

--- 01 Energy Data/src/energyPrediction.py
+++ 01 Energy Data/src/energyPrediction.py
@@ -25,24 +25,13 @@ targetData = energyDataFrame['CO2Emm']
 # The original SVM formulations for Regression (SVR) used parameters C [0, inf) and epsilon[0, inf) to apply a penalty to the optimization for points which were not correctly predicted.
 
 # Best Configuration:
-svr = SVR(C= 1.0, epsilon= 0.2, kernel='linear')
+svr = SVR(C= 1.0, epsilon= 0.4, kernel='linear')
 
 # Cross Validation Score: -0.032 (+/- 0.013)
 # SVR(C=10.0, cache_size=200, coef0=0.0, degree=3, epsilon=0.1, gamma=0.0,
 #   kernel=linear, max_iter=-1, probability=False, random_state=None,
 #   shrinking=True, tol=0.001, verbose=False)
 # Average Absolute Deviation: 0.00136170016638
-
-# NOT USED ANYMORE : cross_val_score supports Cross-Validation-Parameter, which does the same
-# # create kfold
-# n_folds = 10
-# kf = cross_validation.KFold(numKeys, n_folds=n_folds)
-
-#  # fill cross-validation
-# for train_index, test_index in kf:
-#  	#print("TRAIN:", train_index, "TEST:", test_index)
-#  	X_train, X_test = trainingData[train_index], trainingData[test_index]
-#  	y_train, y_test = targetData[train_index], targetData[test_index]
 
 # score
 score = cross_validation.cross_val_score(svr, trainingData, targetData, scoring ="mean_squared_error", cv=10)
