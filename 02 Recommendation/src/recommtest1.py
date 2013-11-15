50661928cdc427284ef04757e1deff960493c284
Steffen Kolb <hi@steffenkolb.de>
2013-11-15 10:27:57 +0100

    CHANGED variable naming
    changed variables for better understanding

--- 02 Recommendation/src/recommtest1.py
+++ 02 Recommendation/src/recommtest1.py
@@ -15,6 +15,9 @@ def topMatches(prefs,person,similarity):
 				results[critic] = similarity(prefs, person, critic, normed=True)
 			else:
 				results[critic] = similarity(prefs, person, critic)
+			#endif
+		#endif
+	#endfor
 
 	# save into dataframe
 	df = pd.DataFrame(results, index=["Similarity"]);
@@ -33,13 +36,14 @@ print "Euclid:\n%s \n\nPearson:\n%s" % (str(result_euclid), str(result_pearson))
 def getRecommendations(prefs,person,similarity):
 	_correlations = topMatches(prefs,person,similarity)
 	_sum = dict()
-	_kSum = dict()
+	_sumCorrelations = dict()
 	#find films which person did not watch
 	for critic in prefs:
 		if critic != person:
 			# critic has negative correlation, ignore him
 			if _correlations.at[critic, "Similarity"] < 0:
 				continue
+
 			# go through all films of that critic
 			for film in prefs[critic]:
 				# if person has not rated the film yet
@@ -47,28 +51,29 @@ def getRecommendations(prefs,person,similarity):
 					# add the movie to the possible moview of not in yet
 					if film not in _sum:
 						_sum[film] = 0.0
-						_kSum[film] = 0
+						_sumCorrelations[film] = 0
 					#endif
 
 					# add the new review-value to the film-value
 					_sum[film] += prefs[critic][film] * _correlations.at[critic, "Similarity"]
 
 					# add correlation of the person to kSum
-					_kSum[film] += _correlations.at[critic, "Similarity"]
+					_sumCorrelations[film] += _correlations.at[critic, "Similarity"]
 				#endif
 			#endfor
 		#endif
 	#endfor
 
-	# divide the sum by the kSum
+	# divide the sum by the summed correlations to get the kSum
+	_kSum = dict()
 	for film in _sum:
-		_sum[film] /= _kSum[film]
+		_kSum[film] = _sum[film] / _sumCorrelations[film]
 
-	# save into dataframe
-	df = pd.DataFrame(_sum, index=["kSum"]);
+	# save into dataframe and transpose
+	df = pd.DataFrame(_kSum, index=["kSum"]).transpose();
 
-	# transpose and sort
-	return df.transpose().sort(columns=["kSum"], ascending=False)
+	# sort and return
+	return df.sort(columns=["kSum"], ascending=False)
 
 recommendations = getRecommendations(rm.critics, "Toby", rm.sim_euclid)
 print "\n %s" % (str(recommendations))
