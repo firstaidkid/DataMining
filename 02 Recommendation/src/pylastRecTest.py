82e3dd33f7ec51192bec465a47ba1f610ecf36be
Steffen Kolb <hi@steffenkolb.de>
2013-11-15 12:28:01 +0100

    UPDATED 3 last.fm Musikempfehlungen

--- 02 Recommendation/src/pylastRecTest.py
+++ 02 Recommendation/src/pylastRecTest.py
@@ -2,8 +2,15 @@ import numpy as np
 import pylast
 
 API_KEY = "b544c9a0541faf9934aef05ab9d48ef3"
-API_SECRET = "c9240638690dea0384baa7e5c0a6b489"
+network=pylast.get_lastfm_network(api_key = API_KEY)
 
-network=pylast.get_lastfm_network(api_key = API_KEY, api_secret = API_SECRET)
-artist = network.get_artist("Backstreet Boys")
-topAlbums = artist.get_top_albums()
+myArtist = "The Rolling Stones"
+
+# get artist
+artist = network.get_artist(myArtist)
+
+# get top fans
+topfans = artist.get_top_fans(10)
+
+# extract users from topfans-answer
+users = [a.item for a in topfans]
