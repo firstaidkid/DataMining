b7aae9f92984e4306f55e7ce663d28f67d25b854
Steffen Kolb <hi@steffenkolb.de>
2013-11-15 12:24:16 +0100

    CHANGED somestuff in lastfm-exercise

--- 02 Recommendation/src/pylastRecTest.py
+++ 02 Recommendation/src/pylastRecTest.py
@@ -4,9 +4,6 @@ import pylast
 API_KEY = "b544c9a0541faf9934aef05ab9d48ef3"
 API_SECRET = "c9240638690dea0384baa7e5c0a6b489"
 
-username = "firstaidkid"
-password_hash = "e9536d7f26ffee297652cdbc2cd2eb40"
-
-network=pylast.get_lastfm_network(api_key = API_KEY, api_secret = API_SECRET, username = username, password_hash = password_hash)
+network=pylast.get_lastfm_network(api_key = API_KEY, api_secret = API_SECRET)
 artist = network.get_artist("Backstreet Boys")
 topAlbums = artist.get_top_albums()
