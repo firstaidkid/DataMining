12af137ed8267765cc0af0b57e3ec4b9519cc997
Steffen Kolb <hi@steffenkolb.de>
2013-11-18 10:40:04 +0100

    UPDATED docclass.py
    added max and minWordlength

--- 03 Spam Filter/src/docclass.py
+++ 03 Spam Filter/src/docclass.py
@@ -1,12 +1,10 @@
 #imports
-import re
 
-
-def getwords(doc):
+def getwords(doc, minWordLength = 3, maxWordLength = 20):
 	words = doc.split()
 	print words
-	cleanedWords= [t.strip("().,:;!?-").lower() for t in words]
+	cleanedWords= [t.strip("().,:;!?-").lower() for t in words if (len(t) > minWordLength) and (len(t) < maxWordLength)]
 	print cleanedWords
 
-getwords("Hello World. Today is a beautiful monday morning (the 18th of November).")
+getwords("Hello World. Today is a beautiful monday morning (the 18th of November). dieseswortistvielzulangsagichdir")
 
