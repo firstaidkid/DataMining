282e1bbef66e9d9400111574f21bf36b72cb9c29
Dirk Fritz <df026@hdm-stuttgart.de>
2013-11-18 10:36:47 +0100

    UPDATED docclass.py
    bla

--- 03 Spam Filter/src/docclass.py
+++ 03 Spam Filter/src/docclass.py
@@ -3,10 +3,12 @@ import re
 
 
 def getwords(doc):
+	wordDict = dict()
 	words = doc.split()
 	print words
-	cleanedWords= [t.strip("().,:;!?-").lower() for t in words]
+	cleanedWords= [t.strip("().,:;!?-").lower() for t in words if (len(t) > 3) and (len(t) <20)]
 	print cleanedWords
+
 
 getwords("Hello World. Today is a beautiful monday morning (the 18th of November).")
 
