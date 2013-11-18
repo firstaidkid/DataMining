1a2fea4ae43d9929ceafabdf3c344cd62d1536d5
Dirk Fritz <df026@hdm-stuttgart.de>
2013-11-18 10:32:23 +0100

    UPDATED docclass.py
    Added first code

--- 03 Spam Filter/src/docclass.py
+++ 03 Spam Filter/src/docclass.py
@@ -1,1 +1,12 @@
-#Hello World
+#imports
+import re
+
+
+def getwords(doc):
+	words = doc.split()
+	print words
+	cleanedWords= [t.strip("().,:;!?-").lower() for t in words]
+	print cleanedWords
+
+getwords("Hello World. Today is a beautiful monday morning (the 18th of November).")
+
