e0c8b0f51097012d8121819ee624e446fafc8323
Steffen Kolb <hi@steffenkolb.de>
2013-12-09 17:52:07 +0100

    Updated Facerecognition
    added Exercise 3

--- 05 Face Recognition/src/faceRecognitionTemplate.py
+++ 05 Face Recognition/src/faceRecognitionTemplate.py
@@ -1,6 +1,8 @@
 from os.path import isdir,join,normpath
 from os import listdir
 
+import sys
+
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 
@@ -8,10 +10,12 @@ import Image
 
 from numpy import asfarray,dot,argmin,zeros, array
 from numpy import average,sort,trace,argsort
+import numpy as np
 from numpy.linalg import svd,eigh
 from numpy import concatenate, reshape, vstack
 from math import sqrt
 
+import Tkinter
 import tkFileDialog
 
 def parseDirectory(directoryName,extension):
@@ -51,6 +55,7 @@ def generateListOfImgs(listOfTrainFiles):
 
         # add to results-list
         result.append(img)
+
     return result
 
 
@@ -75,65 +80,183 @@ def convertImgListToNumpyData(imgList):
 
     return concatenate(imgArrays, axis=0)
 
+def convertImgToNumpyData(img):
+    imgList = list()
+    imgList.append(img)
 
-def calculateEigenfaces(adjfaces, width, height):
-    # number of relevant vectors
-    K = 6
+    numpyList = convertImgListToNumpyData(imgList)
+    return numpyList[0]
 
+
+def calculateEigenfaces(adjfaces, width, height, K):
     # calulate the quadratic matrix MxM
-    quadMatrix = dot(adjfaces, adjfaces.transpose())
+    CV = dot(adjfaces, adjfaces.transpose())
 
     # Calculate Eigenvalues and Eigenvectors
-    eigenValues, eigenVectors = eigh(quadMatrix)
+    eigenValues, eigenVectors = eigh(CV)
 
     # get sortindices for reverse sorting
     sortIndices = argsort(eigenValues)[::-1]
 
     # apply indices to arrays
-    #eigenValues = eigenValues[sortIndices][:K]
-    eigenVectors = eigenVectors[sortIndices][:K]
+    eigenValues = eigenValues[sortIndices]
+    eigenVectors = eigenVectors[sortIndices]
 
-    return eigenVectors
+    # calculate Eigenfaces from Eigenvectors
+    Eigenfaces = dot(adjfaces.transpose(), eigenVectors).transpose()
+
+    # restrict to K faces
+    return Eigenfaces[:K]
 
 
+def subtractAverageImage(ArrayOfFaces, AverageImage):
+    # remove average image from all images
+    NormedArrayOfFaces = ArrayOfFaces.copy()
+    for image in NormedArrayOfFaces:
+        image = image - AverageImage
 
+    return NormedArrayOfFaces
+
+def calculateEigenfaceCoordinates(NormedArrayOfFaces, Usub):
+    EigenfaceCoordinates = np.zeros((Usub.shape[0], NormedArrayOfFaces.shape[0]))
+
+    # go through each of the normed images
+    for i in range(NormedArrayOfFaces.shape[0]):
+        
+        # go through each of the eigenvectors of the eigenface
+        for k in range(Usub.shape[0]):
+
+            # calculate eingeface-coordinate of that image by multiplying it with the eigenvector
+            EigenfaceCoordinates[k,i] = dot(Usub[k,:], NormedArrayOfFaces[i, :])
+
+    return EigenfaceCoordinates.transpose()
+
+
+def calculateEigenfaceCoordinatesForTestimage(NormedTestFace, Usub):
+    TestfaceCoordinates = np.zeros((Usub.shape[0])) #calculateEigenfaceCoordinates(NormedTestFace, Usub)
+
+    # go through each of the eigenvectors of the eigenface
+    for k in range(Usub.shape[0]):
+        # calculate eingeface-coordinate of that image by multiplying it with the eigenvector
+        TestfaceCoordinates[k] = dot(Usub[k,:], NormedTestFace)
+
+    return TestfaceCoordinates
+
+
+# easy way to calculate euclidean distance between two arrays
+def euclideanDist(A, B):
+    return np.linalg.norm(A-B)
+
+def getNearestImage(TestfaceCoordinates, EigenfaceCoordinates):
+    dist = sys.float_info.max
+    nearestImageIdx = 0
+    for idx, faceCoordinates in enumerate(EigenfaceCoordinates):
+        newDist = euclideanDist(TestfaceCoordinates, faceCoordinates)
+        if dist > newDist:
+            nearestImageIdx = idx
+            dist = newDist
+
+    return nearestImageIdx, dist
 
 
 ####################################################################################
 #Start of main programm
 
-#Choose Directory which contains all training images 
+#Choose Directory which contains all training images
+root = Tkinter.Tk()
+root.withdraw()
 TrainDir=tkFileDialog.askdirectory(title="Choose Directory of training images")
 #Choose the file extension of the image files
 Extension='png' 
 #Choose the image which shall be recognized
 testImageDirAndFilename=tkFileDialog.askopenfilename(title="Choose Image to detect")
+root.destroy()
 
 ####################################################################################
 # Implement required functionality of the main programm here
 
 # global height/width
-g_width = 0
-g_height = 0
+g_width = 167
+g_height = 250
 
 # store all training-images in a list
 trainImages = generateListOfImgs(parseDirectory(TrainDir, Extension))
 
+# get numpyArray
+ArrayOfFaces = convertImgListToNumpyData(trainImages)
+
+# get the normed image
+AverageImage = average(ArrayOfFaces, axis=0)
+
+# remove average image from all images
+NormedArrayOfFaces = subtractAverageImage(ArrayOfFaces, AverageImage)
+
+# Calulate Eigenfaces with K = 6
+Usub = calculateEigenfaces(NormedArrayOfFaces, g_width, g_height, 6)
+
+### Show all Eigenfaces as Images
+eigenfaceFig = plt.figure()
+for i in range(Usub.shape[0]):
+        picture = Usub[i, :]
+        PicCopy = np.reshape(picture, (g_height, g_width) )
+        ax = eigenfaceFig.add_subplot(2,3,i)
+        ax.yaxis.set_visible(False)
+        ax.xaxis.set_visible(False)
+        ax.imshow(PicCopy, cmap=plt.cm.gray)
+
+plt.show()
+
+# calculate coordinates of the Normed Images in the Eigenface-Room
+EigenfaceCoordinates = calculateEigenfaceCoordinates(NormedArrayOfFaces, Usub)
+
+
+# 3 Phase Erkennung
 # store the test-image
 testImage = Image.open(testImageDirAndFilename)
 
-# get numpyArray
-NormedArrayOfFaces = convertImgListToNumpyData(trainImages)
+# convert to grayscale
+testImage = testImage.convert('L')
 
-# get the normed image
-normedImage = average(NormedArrayOfFaces, axis=0)
+# get NumpyArray of test-image
+testImageAsNumpy = convertImgToNumpyData(testImage)
 
-# remove average image from all images
-for image in NormedArrayOfFaces:
-    image = image - normedImage
+# remove average from testImage
+NormedTestFace = subtractAverageImage(array(testImageAsNumpy), AverageImage)
 
-# Calulate Eigenfaces with K = 6
-Usub = calculateEigenfaces(NormedArrayOfFaces, g_width, g_height)
+# Calculate Eigenface-Coordinates of TestImage
+TestfaceCoordinates = calculateEigenfaceCoordinatesForTestimage(NormedTestFace, Usub)
 
-# calculate coordinates of the Normed Images in the Eigenface-Room
-NormedArrayOfFaces = dot(Usub, NormedArrayOfFaces)
+# Get nearest testimage and the distance
+NearestImageIndex, dist = getNearestImage(TestfaceCoordinates, EigenfaceCoordinates)
+
+print "NearestImageIndex: %d , dist: %.3f" % (NearestImageIndex, dist)
+
+
+### Show all Eigenfaces as Images
+resultFig = plt.figure()
+resultFig.suptitle("Euclidean Distance %.3f" % (dist))
+
+ax = resultFig.add_subplot(1,2,1)
+ax.yaxis.set_visible(False)
+ax.xaxis.set_visible(False)
+ax.imshow(testImage, cmap=plt.cm.gray)
+
+ax2 = resultFig.add_subplot(1,2,2)
+ax2.yaxis.set_visible(False)
+ax2.xaxis.set_visible(False)
+ax2.imshow(trainImages[NearestImageIndex], cmap=plt.cm.gray)
+
+plt.show()
+
+
+
+
+
+
+
+
+
+
+
+
+
