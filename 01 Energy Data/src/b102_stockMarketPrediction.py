8f929fc398343d82d92c83a66933f2ed2f55f81d
Steffen <sapmme@gmail.com>
2013-12-15 14:53:20 +0100

    UPDATED stockMarketPrediction
    - added cyclic data and fitting
    - not sure about prediction :-(

--- 01 Energy Data/src/b102_stockMarketPrediction.py
+++ 01 Energy Data/src/b102_stockMarketPrediction.py
@@ -1,12 +1,37 @@
+import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.svm import SVR
 from sklearn import cross_validation
 
 
-def createCyclicData(data, timeDealy):
+def createCyclicData(data, timeDelay):
+    # create an array with the shape nrValues X timeDelay+Value
+    result = np.zeros((len(data),timeDelay+1))
 
-    return data
+    # timedelay too small, return current data
+    if timeDelay == 0:
+        return data
+
+    # go through each data-row
+    for i in range(len(data)):
+        #print "i:\t\t%d" % i
+
+        # get the i-timeDelay values as input-attributes
+        for j in range(timeDelay+1):
+            # calculate index of item
+            index = i - (timeDelay-j)
+
+            # ring around the rosy
+            if index < 0:
+                index = len(data) + index
+
+            #print "index:\t%d" % index
+
+            # put value into result-index
+            result[i,j] = data[index]
+
+    return result
 
 
 # read dataframe from file
@@ -22,9 +47,12 @@ plt.show()
 # Choose yahoo
 yahooDf = df['YHOO']
 
-# extract features and targets from data
-features = yahooDf[:650]
-targets = yahooDf[650:]
+timeDelay = 24
+cyclicYahoo = createCyclicData(yahooDf, timeDelay)
+
+# extract features and targets from data, use first 650 rows for training
+features = np.array(cyclicYahoo[:650,:timeDelay])
+targets = cyclicYahoo[:650, timeDelay:].reshape((650))
 
 # Create SVR with given settings
 svr = SVR(C= 500.0, epsilon= 0.3, kernel='rbf')
@@ -32,12 +60,16 @@ svr = SVR(C= 500.0, epsilon= 0.3, kernel='rbf')
 # start fitting
 fittedData = svr.fit(features, targets)
 
+# data to predict: last 30 days
+predict = cyclicYahoo[len(cyclicYahoo)-30:, :timeDelay]
+
 # predict the data using trainings-data from the SVR
-predictedData = svr.predict(features)
+predictedData = svr.predict(predict)
 
 # calculate the absolute deviation
-absDeviation = predictedData - targets
+absDeviation = predictedData - targets[len(targets)-30:]
 meanAbsDevitation = absDeviation.mean()
 if meanAbsDevitation < 0:
     meanAbsDevitation *= -1
 print "\nMean Absolute Deviation: %0.3f" % meanAbsDevitation
+
